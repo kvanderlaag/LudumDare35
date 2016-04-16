2e99e3b3ecb26a19fc5a20f40382d92de5e5b36c
Evan <e.w.nickerson@gmail.com>
2016-04-15 20:26:22 -0700

    added monster health component

---
+++ Assets/Scripts/HealthComp.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+public enum EDamageType
+{
+	WEREWOLF,
+	VAMPIRE,
+	ALIEN
+}
+
+public struct DamageInfo
+{
+	public EDamageType damageType;
+	public int damageAmount;
+	public bool bReveals;
+}
+
+public class HealthComp : Monobehavior
+{
+	public int maxHealth { get; private set; }
+	public int curHealth { get; private set; }
+
+	public EDamageType monsterType;
+	public bool bIsRevealed { get; private set; }
+
+	public void TakeDamage(DamageInfo damageInfo)
+	{
+		//reveal monster if damage type is correct
+		if (!bIsRevealed && damageInfo.damageType == monsterType && damageInfo.bReveals)
+		{
+			RevealMonster();
+		}
+
+		if (damageInfo.damageType == monsterType)
+		{
+			curHealth -= damageInfo.damageAmount;
+			if (curHealth < 0)
+			{
+				MonsterDies();
+			}
+		}
+		else
+		{
+			curHealth -= damageInfo.damageAmount * 0.25f;
+			if (curHealth < 0)
+			{
+				MonsterDies();
+			}
+		}
+	}
+
+	private void RevealMonster()
+	{
+		bIsRevealed = true;
+		//do revealing stuff
+	}
+
+	public void MonsterDies()
+	{
+		curHealth = 0;
+		//monster dying things happen
+	}
+}
