a35c9cf36de9ec7da065c4fccc4599b3a3dfc1ef
Evan <e.w.nickerson@gmail.com>
2016-04-16 12:50:03 -0700

    added skele code for player powers

---
+++ Assets/Scripts/PlayerPowers.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerPowers : MonoBehaviour
+{
+	// shared cooldown and timer of weak powers (strong powers have own cooldown
+	public float weakPowerSharedCD;
+	private float weakPowerTimerCur;
+
+	public Power[] powers;
+	public int currentPowerIdx;
+
+	//how does this work?
+	GameObject remoteR;
+	GameObject remoteL;
+
+	public void AttemptExecutePower()
+	{
+		powers[currentPowerIdx].Execute(remoteR.transform.position);
+	}
+
+	
+}
---
+++ Assets/Scripts/Power.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Power : MonoBehaviour
+{
+	public DamageInfo damage;
+	public float aoeRadius;
+	public bool bIsWeak;
+
+	public float cooldown;
+	private float cooldownTimerCur;
+
+	virtual public void Execute(Vector3 location)
+	{
+		Collider[] aoeCollisions = Physics.OverlapSphere(location, aoeRadius);
+
+		foreach (Collider c in aoeCollisions)
+		{
+			//apply damage using TakeDamage(damage) to all objects with healthcomp
+		}
+	}
+
+	virtual public void Update()
+	{
+		if (cooldownTimerCur > 0f)
+		{
+			cooldownTimerCur -= Time.deltaTime;
+		}
+
+		if (cooldownTimerCur < 0f)
+		{
+			cooldownTimerCur = 0f;
+			
+		}
+	}
+
+	public bool IsReady()
+	{
+		return (cooldownTimerCur > 0f)? false : true;
+	}
+}
