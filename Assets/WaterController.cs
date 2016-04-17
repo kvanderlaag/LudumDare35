cd79e9ec7da40880adae71d4e6745ff59e4af741
Keegan van der Laag <jkvander@uvic.ca>
2016-04-16 19:10:22 -0700

    Water
    Started adding water

---
+++ Assets/WaterController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterController : MonoBehaviour {
+
+    public float displacement = 0.02f;
+
+    private float startOffset;
+
+	// Use this for initialization
+	void Start () {
+        startOffset = Random.Range(-1, 1);
+        transform.position = new Vector3(transform.position.x, startOffset, transform.position.y);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
