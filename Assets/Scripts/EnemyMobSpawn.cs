23a1d1dcea925befb308f6aa7280466ab467839c
Evan <e.w.nickerson@gmail.com>
2016-04-16 18:57:21 -0700

    created enemy mob placement script

---
+++ Assets/Scripts/EnemyMobSpawn.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMobSpawn : MonoBehaviour {
+
+	public GameObject enemyPrefab;
+
+	public void Awake()
+	{
+		SpawnMobs(7);
+	}
+
+	public void SpawnMobs(int enemyCount)
+	{
+		if (enemyCount < 1)
+		{
+			Debug.Log("Spawning empty mob!");
+			return;
+		}
+
+		int enemiesSpawned = 0;
+
+		Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+		enemiesSpawned++;
+
+		GameObject tempTransform = Instantiate(new GameObject()); //used to spawn enemies
+		bool offset = false;
+		int curRing = 1;
+		while (enemiesSpawned < enemyCount)
+		{
+			tempTransform.transform.position = transform.position + new Vector3(0f,0f,-0.6f) * curRing;
+			
+			if (offset)
+			{
+				tempTransform.transform.RotateAround(transform.position, transform.up, 30);
+			}
+
+			for (int i = 0; i < 6; i++)
+			{
+				Instantiate(enemyPrefab, tempTransform.transform.position, Quaternion.identity);
+
+				tempTransform.transform.RotateAround(transform.position, transform.up, 60);
+
+				enemiesSpawned++;
+				if (enemiesSpawned >= enemyCount)
+					break;
+			}
+			offset = (offset)? false : true;
+			curRing++;
+		}
+	}
+}
