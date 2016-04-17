6f9c2ac7dd78e3907125377b905c4a2e94c1b0d0
Evan <e.w.nickerson@gmail.com>
2016-04-17 12:08:07 -0700

    added buildable script to be attached to hex tiles

---
+++ Assets/Scripts/Buildable.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Buildable : MonoBehaviour
+{
+	public GameState gameState;
+	public GameObject builtObject;
+
+	// selects this tile
+	public void SelectTile()
+	{
+		if (builtObject == null && gameState.phaseState == EPhaseState.UPKEEP)
+		{
+			//bring up build options
+		}
+
+		else if (buildObject != null)
+		{
+			//select tower and bring up shapeshift options
+		}
+	}
+}
