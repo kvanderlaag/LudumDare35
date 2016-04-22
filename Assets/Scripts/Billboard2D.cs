97b286c2a4fa9b1f1e5e7e4494c5d84767d9522f
Evan <e.w.nickerson@gmail.com>
2016-04-22 12:38:21 -0700

    added a rain particle effect (wip)

---
+++ Assets/Scripts/Billboard2D.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Billboard2D : MonoBehaviour
+{
+	void Update()
+	{
+		transform.LookAt(
+				Camera.main.transform.position + 2*(transform.position - Camera.main.transform.position), Vector3.up);
+	}
+}
